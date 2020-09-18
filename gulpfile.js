4e1ba3ae254827d30a4664e9b6c6fcd87cb0f24c
marina4952555 <50374240+marina4952555@users.noreply.github.com>
2020-09-18 22:30:32 +0600

    ВНесение правок
    Исправлена сборка, появилась папка docs, сделана оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 "use strict";
 
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.mozjpeg({progressive: true})
+  ]))
+  pipe(gulp.dest("source/img"));      
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,22 +29,54 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("docs/css"))
     .pipe(server.stream());
+});
+
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/*.html",
+      "source/js/**",
+      "source/*.ico"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("docs"));
+});
+
+gulp.task("clean", function () {
+  return del("docs/**");
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "docs/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
+  gulp.watch("source/*.html", gulp.series("build", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("build", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css"
+));
+
+gulp.task("start", gulp.series("build", "server"));
